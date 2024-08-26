4ce893b222f855de2d9bf05393ecf33c23259a89
Aquiles Zeus <168115487+MysteriumXZ@users.noreply.github.com>
2024-08-26 15:11:53 -0300

    Add files via upload
    Versão atualizada do select

--- read.php
+++ read.php
@@ -1,38 +1,48 @@
-<?php 
+<?php
 
-    include("conexao.php");
+include("conexao.php");
 
-    $select = "SELECT * FROM produto";
+$select = "SELECT * FROM produto";
 
-    $resultado = $conn->query($select);
+$resultado = $conn->query($select);
 
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listar</title>
 </head>
+
 <body>
-    <table>
-        <thead>
-            <th>id </th> <th>Nome </th> <th>Preço</th>
-        </thead>
-        <tbody>
-            <?php 
-                while($produto = $resultado->fetch_object()){
-                    echo "<tr>";
-                        echo "<td>$produto->id_produto </td> </td>$produto->nome </td> </td>$produto->preco </td>";
-                    echo "</tr>";
-                }
-            
-           
-            ?>
-        </tbody>
-        
-    </table>
+    <a href="index.php">
+        < voltar</a>
+            <br>
+            <br>
+            <table border="1">
+                <thead>
+                    <th>id </th> <th>Nome </th> <th>Preço</th> <th>Ação</th>
+                </thead>
+                <tbody>
+                    <?php
+                    while ($produto = $resultado->fetch_object()) {
+                        echo "<tr>";
+                            echo "<td>".$produto->id_produto ."</td>";
+                            echo "<td>".$produto->nome ."</td>";
+                            echo "<td>".$produto->preco. "</td>";
+                        echo "</tr>";
+
+                        echo "<td>";
+                            echo "<a href='delete.php?id=$produto->id_produto'>Excluir</a>";
+                        echo "</td>";
+                    }
+
+
+                    ?>
+                </tbody>
+
+            </table>
 </body>
+
 </html>
